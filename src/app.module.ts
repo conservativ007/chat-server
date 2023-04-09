25e760523984b5797b10bc7c8df9d7571a33a826
Maks <alisa3d2007@gmail.com>
2023-04-09 17:14:53 +0300

    feat: adde exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,18 @@ import { MessagesModule } from './messages/messages.module';
 import { newOrmConfig } from './common/typeOrm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { APP_FILTER } from '@nestjs/core';
+import { CustomExceptionFilter } from './common/exceptions/CustomExceptionFilter';
 
 @Module({
   imports: [TypeOrmModule.forRoot(newOrmConfig), MessagesModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: CustomExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
